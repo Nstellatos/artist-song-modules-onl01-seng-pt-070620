3288507e7194e15efe37f60b7610bf3ea690c42a
Nicole Stellatos <stellatosnicole@gmail.com>
2020-08-08 20:32:01 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,13 +1,17 @@
 module Memorable
-  
-    def all
-      self.class
-    end
+  module ClassMethods
     def reset_all
       self.all.clear
     end
-
+ 
     def count
       self.all.count
     end
   end
+ 
+  module InstanceMethods
+    def initialize
+      # some more code coming soon!
+    end
+  end
+end
